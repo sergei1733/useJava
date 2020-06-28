623cd4ded9db05523e0bc23a953b7ea7e920c182
Sergei <sergei1733@yandex.ru>
2020-06-28 18:38:31 +0400

    "222222222222"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Я стану программистом java!!!!1111");
+	System.out.println("!!!!!!!!!!!!!!!");
     }
 }
