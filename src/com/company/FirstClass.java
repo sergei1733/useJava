6e643ddb3e8c38d966d83ee20d1e5762875f7583
Sergei <sergei1733@yandex.ru>
2020-06-28 18:40:36 +0400

    "222222222222"

--- src/com/company/FirstClass.java
+++ src/com/company/FirstClass.java
@@ -3,6 +3,6 @@ package com.company;
 public class FirstClass {
 
     public static void main(String[] args) {
-        System.out.println("-------------------");
+        System.out.println("------------------////-");
     }
 }
