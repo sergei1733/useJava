9c8efe9fa7ca186877805598f68e6277aadb54fe
Sergei <sergei1733@yandex.ru>
2020-06-29 20:26:59 +0400

    "222222222222"

--- src/com/company/FirstClass.java
+++ src/com/company/FirstClass.java
@@ -1,8 +1,18 @@
 package com.company;
-
+        //3.1
+        //Связь между температурой по шкале Цельсия и температурой по шкале Фаренгейта
+        //выражается формулой: C=S(F-32)/9, где С - температура
+        //по шкале Цельсия, F - температура по шкале Фаренгейта.
+        //Напишите класс, который принимает с клавиатуры температуру по
+        //шкале Фаренгейта, а затем выводит на экран эту же температуру, но по
+        //шкале Цельсия.
+import java.util.Scanner;
 public class FirstClass {
-
+    public static Scanner in=new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("------------------////-");
+        double tempf,tempC;
+        tempf=in.nextDouble();
+        tempC=0.5*(tempf-31)/9;
+        System.out.println("Температура по шкале Цельсия равна "+tempC);
     }
 }
