e234fdb8fb8a4302af45cb60dcb3c63f53fc6775
Sergei <sergei1733@yandex.ru>
2020-06-29 20:49:41 +0400

    "222222222222"

---
+++ src/com/company/Task3_3.java
@@ -0,0 +1,23 @@
+package com.company;
+        /*
+        Во многих странах для измерения небольших размеров используется
+        не только единица длины «сантиметр», но и такая единица, как
+        «дюйм» (1 дюйм= 2.54 сантиметра).
+        Напишите класс, который принимает с клавиатуры значение, равное
+        размеру в дюймах, и затем подсчитывает и выводит на экран это же
+        значение, но в сантиметрах.
+         */
+import java.util.Scanner;
+
+public class Task3_3 {
+    public static Scanner in=new Scanner(System.in);
+    public static void main(String[] args) {
+        double sm,duim;
+        System.out.print("введите дюймы ");
+        duim=in.nextDouble();
+        sm=duim/2.54;
+        System.out.println("значение в сантиметрах равно "+sm);
+
+
+    }
+}
