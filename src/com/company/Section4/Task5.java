a5fcb11ec959c3dfbaf6788c5449a8b0056745eb
Sergei <sergei1733@yandex.ru>
2020-07-01 09:31:06 +0400

    4.5

---
+++ src/com/company/Section4/Task5.java
@@ -0,0 +1,20 @@
+package com.company.Section4;
+        /*
+    Напишите класс, который принимает с клавиатуры целое положительное двузначное
+    число и выводит его на экран в «полном виде»: например, для числа 34 это
+    будет выглядеть как 30+4.
+         */
+
+import java.util.Scanner;
+
+public class Task5 {
+    public static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int a, a1, a2;
+        a = in.nextInt();
+        a1 = a-(a%10);
+        a2 = a%10;
+        System.out.println(a1 + "+" + a2);
+    }
+}
