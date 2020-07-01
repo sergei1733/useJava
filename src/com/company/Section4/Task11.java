c5843f3805679af9edaebcdebeded13aaf3d7c25
Sergei <sergei1733@yandex.ru>
2020-07-01 12:06:25 +0400

    "11"

--- src/com/company/Section4/Task11.java
+++ src/com/company/Section4/Task11.java
@@ -12,11 +12,11 @@ public class Task11 {
     public static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int a, a1, a2, a3, a4;
+        int a, a1, a2, a3;
         a = in.nextInt();
-        a1 = a-a%10;
-        a2 = a%10;
-        a3 = a1-a2;
+        a1 = a - a % 10;
+        a2 = a % 10;
+        a3 = a1 - a2;
         System.out.println(a3);
     }
 }
