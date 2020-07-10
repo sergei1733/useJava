e703c73f695181cc3fe3714c67e36d851b24fc1c
Sergei <sergei1733@yandex.ru>
2020-07-10 21:12:55 +0400

    "5.4"

---
+++ src/com/company/Section5/Task54.java
@@ -0,0 +1,22 @@
+package com.company.Section5;
+        /*
+    Дано логическое выражение:
+(z>x)||(x<O)&&(z-y>9)
+
+Чему равно значение этого выражения для следующих значений переменных:
+x=-2;y=5;z=13? ЛОЖЬ
+         */
+
+import java.util.Scanner;
+
+
+public class Task54 {
+    public static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
