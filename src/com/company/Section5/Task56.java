c3716744a151d83722879f2a29ec06ec96f57ccb
Sergei <sergei1733@yandex.ru>
2020-07-10 21:26:34 +0400

    "5_6 "

---
+++ src/com/company/Section5/Task56.java
@@ -0,0 +1,21 @@
+package com.company.Section5;
+        /*
+   Дано логическое выражение:
+((z>x)||(x<O))&&(z-y>9)
+Чему равно значение этого выражения для следующих значений переменных:
+х=-2; y=5; z=13?  выражение примет значение false так как правое выражение ложноле
+         */
+
+import java.util.Scanner;
+
+
+public class Task56 {
+    public static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
