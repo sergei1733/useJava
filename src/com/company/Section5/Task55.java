0cbc0725bd00f131dfad1456a3ba0e1a0060f1cb
Sergei <sergei1733@yandex.ru>
2020-07-10 21:23:11 +0400

    "5_5"

---
+++ src/com/company/Section5/Task55.java
@@ -0,0 +1,29 @@
+package com.company.Section5;
+        /*
+    Перед вами фрагмент программы:
+Scannerin=newScanner(System.in);
+a=in.nextlnt();
+b=in.nextlnt();
+if((a<b)||(a<1OO)
+System.out.println("TRUE");
+else
+System.out.println("FALSE");
+Выберите значение для ввода в переменную а и переменную Ь, так,
+чтобы было выведено сообщение FALSE. Объясните свой выбор.
+a = 101
+b = 50   чтобы было выведено сообщение false нам необнодимо сделать ложными оба выражения
+         */
+
+import java.util.Scanner;
+
+
+public class Task55 {
+    public static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
