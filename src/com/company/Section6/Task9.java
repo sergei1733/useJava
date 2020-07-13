d80fd445f29677e0943a90e7149332a88dedff66
Sergei <sergei1733@yandex.ru>
2020-07-13 23:55:44 +0400

    "6_9"

---
+++ src/com/company/Section6/Task9.java
@@ -0,0 +1,23 @@
+package com.company.Section6;
+        /*
+        Напишите программу (класс), которая принимает с клавиатуры двузначное число
+        и проверяет, что больше: само это число или произведение его цифр.
+*/
+
+import java.util.Scanner;
+
+
+public class Task9 {
+    public static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int a,a1,a2,a3,c;
+        a = in.nextInt();
+        a1 = a / 10;
+        a2 = a % 10;
+        if ((a1+a2)>(a1*a2)) System.out.println("положительное");
+
+
+
+    }
+}
