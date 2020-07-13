8e0f25c804e3891a4420e422d46fd65a34bdb838
Sergei <sergei1733@yandex.ru>
2020-07-13 21:50:12 +0400

    "5_6 "

---
+++ src/com/company/Section6/Task1.java
@@ -0,0 +1,41 @@
+package com.company.Section6;
+        /*
+   import java.util.;
+class IfSamplel
+{
+}
+static Scanner reader = new Scanner(System.in);
+puЬlic static void main(String[) args)
+{
+}
+int х = reader.nextlnt();
+int y=reader.nextlnt();
+if (х <у) System.out.println ("х меньше у");
+if (х ==у) System.out.println ("х теперь равна у");
+if (х >у) System.out.println ("х теперь больше у");
+
+1. Что будет выведено на экран, если с клавиатуры введены значения 5 и 9?   х меньше у
+2. Что будет выведено на экран, если с клавиатуры введены значения 9 и 9?   х теперь равна у
+3. Что будет выведено на экран, если с клавиатуры введены значения 9 и 5?   х теперь больше у
+4. Приведите пример вводимых с клавиатуры данных, для которых
+на экран будет выведено сообщение х меньше у.                       1 и 3
+5. Приведите пример вводимых с клавиатуры данных, для которых
+на экран будет выведено сообщение х теперь больше у.                4 и 1
+6. Приведите пример вводимых с клавиатуры данных, для которых
+на экран будет выведено сообщение х теперь меньше у.                2 и 3
+
+         */
+
+import java.util.Scanner;
+
+
+public class Task1 {
+    public static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
