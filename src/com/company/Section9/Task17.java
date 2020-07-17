e35e600634d96f4ff6845948084a8274913fc01f
Sergei <sergei1733@yandex.ru>
2020-07-17 15:35:17 +0400

    "9 17"

---
+++ src/com/company/Section9/Task17.java
@@ -0,0 +1,30 @@
+package com.company.Section9;
+        /*
+    Задание 9.17
+Напишите класс, который принимает с клавиатуры три числа и проверяет, являются ли эти числа углами
+одного и того же треугольника. По результатам проверки следует вывести на экран соответствующее
+текстовое сообщение.
+*/
+
+import java.util.Scanner;
+
+
+public class Task17 {
+    public static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int a, b, c;
+        a = in.nextInt();
+        b = in.nextInt();
+        c = in.nextInt();
+
+       // if (a+b+c < 180)
+            System.out.println(a+b+c <= 180? "Является":"не является");
+       // else
+            //System.out.println("не является");
+
+
+
+    }
+}
