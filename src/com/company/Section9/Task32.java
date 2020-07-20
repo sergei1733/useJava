8247d92dbdd6a0098210f1c5130f4a6b46d21dfc
Sergei <sergei1733@yandex.ru>
2020-07-20 20:12:04 +0400

    "9 32"

---
+++ src/com/company/Section9/Task32.java
@@ -0,0 +1,14 @@
+package com.company.Section9;
+import java.util.Scanner;
+/*
+
+ */
+public class Task32 {
+    public static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int a,a1,a2,a3;
+        a = in.nextInt();
+        System.out.println(a%4==0?"да":"нет");
+    }
+}
