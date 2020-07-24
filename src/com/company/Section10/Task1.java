67f76476c141813407efc48cc15771402bc2e314
Sergei <sergei1733@yandex.ru>
2020-07-24 15:14:15 +0500

    "10 1"

---
+++ src/com/company/Section10/Task1.java
@@ -0,0 +1,15 @@
+package com.company.Section10;
+import java.lang.Math;
+/*
+Задание 10.1
+Напишите класс, который генерирует и выводит на экран случайное
+целое чисел из диапазона положительных двузначных чисел.
+ */
+public class Task1 {
+
+    public static void main(String[] args) {
+        double a = Math.random()*100;
+        System.out.println(a);
+
+    }
+}
