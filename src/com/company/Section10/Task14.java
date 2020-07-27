933e4de01773dfaa9ea33d50a555c922b844b919
Sergei <sergei1733@yandex.ru>
2020-07-27 21:42:22 +0500

    10 14

--- src/com/company/Section10/Task14.java
+++ src/com/company/Section10/Task14.java
@@ -1,5 +1,7 @@
 package com.company.Section10;
+
 import java.lang.Math;
+
 /*
 Задание 10.14
 Напишите класс, который создает и выводит на экран 5 случайных целых чисел, заканчивающихся на
@@ -7,10 +9,10 @@ import java.lang.Math;
  */
 public class Task14 {
     public static void main(String[] args) {
-        int start=10, end = 90, a,i=5;
-        while (i>0){
-            a = start + (int)((end-start+1)*Math.random());
-            if (a%10==0){
+        int start = 10, end = 90, a, i = 5;
+        while (i > 0) {
+            a = start + (int) ((end - start + 1) * Math.random());
+            if (a % 10 == 0) {
                 i--;
                 System.out.println(a);
             }
---
+++ src/com/company/Section10/Task15.java
@@ -0,0 +1,7 @@
+package com.company.Section10;
+import java.lang.Math;
+/*
+
+ */
+public class Task15 {
+}
