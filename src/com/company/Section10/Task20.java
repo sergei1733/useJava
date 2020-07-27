37d2b24f7e07406cf76f16900976c1e0a0cc89fa
Sergei <sergei1733@yandex.ru>
2020-07-27 23:36:29 +0500

    10 20

--- src/com/company/Section10/Task20.java
+++ src/com/company/Section10/Task20.java
@@ -1,5 +1,7 @@
 package com.company.Section10;
+
 import java.lang.Math;
+
 /*
 Задание 10.20
 Напишите класс, который генерирует и выводит на экран сначала случайное положительное однозначное
@@ -9,26 +11,26 @@ import java.lang.Math;
  */
 public class Task20 {
     public static void main(String[] args) {
-        int start = 1, end = 10, a,b,b1,b2,b3,max = 0,c;
-        a = start+(int)(end*Math.random());
+        int start = 1, end = 10, a, b, b1, b2, b3, max = 0, c;
+        a = start + (int) (end * Math.random());
         System.out.println(a);
         start = 100;
         end = 999;
-        b = start + (int)((end-start+1)*Math.random());
+        b = start + (int) ((end - start + 1) * Math.random());
         System.out.println(b);
-        b1 = b/100;
-        b2 = b%100/10;
-        b3 = b%10;
-        System.out.println(b1+" "+b2+" "+b3);
-        if (b1>b2 && b1>b3) max = b1;
-        if (b2>b1 && b2>b3) max = b2;
-        if (b3>b1 && b3>b2) max = b3;
+        b1 = b / 100;
+        b2 = b % 100 / 10;
+        b3 = b % 10;
+        System.out.println(b1 + " " + b2 + " " + b3);
+        if (b1 > b2 && b1 > b3) max = b1;
+        if (b2 > b1 && b2 > b3) max = b2;
+        if (b3 > b1 && b3 > b2) max = b3;
 
-        if (max>a)
-            c = max-a;
+        if (max > a)
+            c = max - a;
         else
-            c = a-max;
-        System.out.println("разница = "+c);
+            c = a - max;
+        System.out.println("разница = " + c);
 
 
     }
