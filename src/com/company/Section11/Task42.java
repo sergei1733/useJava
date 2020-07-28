65740d621bdc68ed6d473ba1070e6a6138c0c54a
Sergei <sergei1733@yandex.ru>
2020-07-28 23:36:59 +0500

    11 43

--- src/com/company/Section11/Task42.java
+++ src/com/company/Section11/Task42.java
@@ -1,4 +1,5 @@
 package com.company.Section11;
+
 /*
 Задание 11.42
 Напишите класс, который выводит на экран все целые трехзначные
@@ -7,14 +8,14 @@ package com.company.Section11;
  */
 public class Task42 {
     public static void main(String[] args) {
-        int a1,a2,a3,summ;
-        for (int i=-999;i<=999;i++){
-            a1 = i/100;
-            a2 = (i%100)/10;
-            a3 = i/10;
-            summ = a1+a2+a3;
-            if (summ!=0)
-            System.out.print(i%summ==0?i+" ":"");
+        int a1, a2, a3, summ;
+        for (int i = -999; i <= 999; i++) {
+            a1 = i / 100;
+            a2 = (i % 100) / 10;
+            a3 = i / 10;
+            summ = a1 + a2 + a3;
+            if (summ != 0)
+                System.out.print(i % summ == 0 ? i + " " : "");
         }
     }
 }
