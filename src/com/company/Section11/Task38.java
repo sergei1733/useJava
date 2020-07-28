522e1bed30d2bc054b9d6857ca8c38fe0a06a267
Sergei <sergei1733@yandex.ru>
2020-07-28 23:08:29 +0500

    11 38

---
+++ src/com/company/Section11/Task38.java
@@ -0,0 +1,33 @@
+package com.company.Section11;
+import java.lang.Math;
+import java.util.Scanner;
+/*
+Задание 11.38
+Напишите класс, который принимает с клавиатуры целое число, а затем генерирует 15 случайных чисел
+из диапазона положительных трехзначных чисел.Для тех сгенерированных чисел, которые делятся на
+введенное с клавиатуры значение, следует вывести на экран округленный в большую сторону квадратный
+корень из первой цифры (число сотен), для остальных - округленный в меньшую сторону квадратный корень из
+второй цифры (число десятков).
+ */
+public class Task38 {
+    public static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        int  start = 100,end = 999, x,a,a1,a2,a3;
+        x = in.nextInt();
+        for (int i=1;i<=15; i++){
+            a = start + (int)((end-start+1)*Math.random());
+            a1 = a/100;
+            a2 = (a%100)/10;
+            if (a%x==0) {
+                System.out.println(Math.ceil(Math.sqrt(a1)) + " " + a);
+            }
+                System.out.println(Math.floor(Math.sqrt(a2))+" "+a);
+
+
+
+
+        }
+
+
+    }
+}
